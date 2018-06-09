213e1d99230c9b6e11af7148c3a4e27c8dfcb88b
xue99999 <1090374167@qq.com>
2018-06-09 15:49:03 +0800

    init

---
+++ github.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+import scrapy
+
+class GithubSpider(scrapy.Spider):
+    name = 'github-spider'
+
+    @property
+    def start_urls(self):
+
+        url_tmp = 'http://github.com/shiyanlou?page={}&tab=repositories'
+
+        urls = (url_tmp.format(i) for i in range(1, 5))
+        
+        return urls
+
+    def parse(self, response):
+        for gitItem in response.css('li.public'):
+            yield {
+                    'name': gitItem.xpath('.//a[@itemprop="name codeRespository"]/text()').re_first("\n\s*(.*)"),
+
+                    'update_time': gitItem.xpath('.//relative-time/@datetime').extract_first()
+                    }
